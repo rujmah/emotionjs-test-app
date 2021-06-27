f6b5716c8a6e8877fae1f7005ae6bf8da95fd52a
Robin Mayfield <rjjm@rjjm.net>
2021-06-27 13:47:33 +0100

    Added modal

--- src/components/Box/Box.styles.jsx
+++ src/components/Box/Box.styles.jsx
@@ -4,8 +4,9 @@ export const BoxContainer = styled.div`
   border-style: solid;
   border-width: medium;
   border-radius: 0.25rem
-  background-color: #333;
-  color: red;
+  background-color: rgba(93,153,253);
+  color: 	#fdc15d;
   padding: 0.25rem;
   width: 40vw;
+  border-radius: 0.25rem;
 `;
--- src/components/Modal/Modal.styles.jsx
+++ src/components/Modal/Modal.styles.jsx
@@ -1,11 +1,17 @@
 import styled from "@emotion/styled";
 
-export const ModalStyles = styled.div`
+export const ModalContainer = styled.div`
   border-style: solid;
   border-width: medium;
-  border-radius: 0.25rem
-  background-color: #333;
-  color: red;
+  border-radius: 0.25rem;
+  background-color: #fdc15d;
+  color: #5d99fd;
   padding: 0.25rem;
   width: 40vw;
+  border-radius: 0.25rem;
+`;
+
+export const ModalBackdrop = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1040;
 `;
